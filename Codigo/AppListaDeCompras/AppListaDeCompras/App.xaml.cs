355497397f7a630805160aebae4777792d59a753
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-11-28 17:52:39 -0300

    S02E07 - FirstPage - Tela de Boas Vindas

--- Codigo/AppListaDeCompras/AppListaDeCompras/App.xaml.cs
+++ Codigo/AppListaDeCompras/AppListaDeCompras/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AppListaDeCompras
+﻿using AppListaDeCompras.Views;
+
+namespace AppListaDeCompras
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new FirstPage();
         }
     }
 }
---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/Views/FirstPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppListaDeCompras.Views;
+
+public partial class FirstPage : ContentPage
+{
+	public FirstPage()
+	{
+		InitializeComponent();
+	}
+}
