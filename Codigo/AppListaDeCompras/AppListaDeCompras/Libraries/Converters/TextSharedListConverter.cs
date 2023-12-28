e4171221b424b2c55bdb862416bedf810b7c4189
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-28 12:13:18 -0300

    S02E15 - Linha Duplicada e Texto Lista Compartilhada

---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/Libraries/Converters/TextSharedListConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.Libraries.Converters
+{
+    public class TextSharedListConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            int? usersCount = (int?)value;
+
+            if (!usersCount.HasValue)
+                return null;
+
+            return usersCount >= 2 ? "(lista compartilhada)" : string.Empty;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
