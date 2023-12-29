6bef930ae8c07c8681d8722bfbfac51cf752d2a0
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-29 15:03:40 -0300

    S02E18 - Estilizando o Popup - Parte 2

---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/ViewModels/Popups/ListToBuySharePageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Mopups.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.ViewModels.Popups
+{
+    public partial class ListToBuySharePageViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private void Close()
+        {
+            MopupService.Instance.PopAsync();
+        }
+    }
+}
