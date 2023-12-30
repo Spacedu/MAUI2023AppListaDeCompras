ece43e22e182543ad8e802ba0d8cb76cd5b58665
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-30 11:47:15 -0300

    S02E23 - Tela - Lista de Itens - Listando Itens - Parte 3

---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/ViewModels/ListOfItensPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using AppListaDeCompras.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.ViewModels
+{
+    [QueryProperty(nameof(ListToBuy), "ListToBuy")]
+    public partial class ListOfItensPageViewModel : ObservableObject
+    {
+        private ListToBuy? _listToBuy;
+        public ListToBuy? ListToBuy
+        {
+            get => _listToBuy;
+            set => SetProperty(ref _listToBuy, value);
+        }
+    }
+}
