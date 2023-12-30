975000f1968eb3529050011a1c7a3a5c6291771a
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-30 15:30:51 -0300

    S02E30 - Tela - Lista de Itens - Cor do Preco Total Conversor - Parte 10

---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/Libraries/Converters/ColorTotalPriceOfItensInCartConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.Libraries.Converters
+{
+    public class ColorTotalPriceOfItensInCartConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            bool hasCaught = (bool)value!;
+            
+            return (hasCaught) ? Colors.Black : Colors.Gray;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
