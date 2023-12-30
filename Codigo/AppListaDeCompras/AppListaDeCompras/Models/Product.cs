90686ac10ced4f6c88e91a6995043a7250e90a5d
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-30 13:57:01 -0300

    S02E26 - Tela - Lista de Itens - Estilizando - Parte 6

--- Codigo/AppListaDeCompras/AppListaDeCompras/Models/Product.cs
+++ Codigo/AppListaDeCompras/AppListaDeCompras/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace AppListaDeCompras.Models
 {
-    public class Product
+    public partial class Product : ObservableObject
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
@@ -14,6 +15,7 @@ namespace AppListaDeCompras.Models
         //TODO - Trocar para Enum
         public string QuantityUnitMeasure { get; set; }
         public decimal Price { get; set; }
-        public bool HasCaught { get; set; } = false;
+        [ObservableProperty]
+        private bool hasCaught = false;
     }
 }
