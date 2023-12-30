ecec3f160ae3fd2c082d678dc84bf5452d0589ba
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-12-30 15:26:26 -0300

    S02E28 - Tela - Lista de Itens - Preco Total Conversor - Parte 8

---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/Libraries/Converters/TextTotalPriceOfItemInCartConverter.cs
@@ -0,0 +1,41 @@
+﻿using AppListaDeCompras.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.Libraries.Converters
+{
+    public class TextTotalPriceOfItemInCartConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var product = value as Product;
+
+            if(product == null)
+            {
+                return "R$ 0,00";
+            }
+
+            if (product.HasCaught)
+            {
+                return (product.Quantity * product.Price).ToString("C");
+            }
+            else
+            {
+                if(product.Price > 0)
+                {
+                    return product.Price.ToString("C") + " " + product.QuantityUnitMeasure;
+                }
+            }
+            return "R$ 0,00";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Codigo/AppListaDeCompras/AppListaDeCompras/ViewModels/ListToBuyViewModel.cs
+++ Codigo/AppListaDeCompras/AppListaDeCompras/ViewModels/ListToBuyViewModel.cs
@@ -32,7 +32,8 @@ namespace AppListaDeCompras.ViewModels
                         new Product { Name = "Arroz 5Kg", Quantity = 10, QuantityUnitMeasure = Models.Enums.UnitMeasure.Un, Price = 28.99m, HasCaught = true },
                         new Product { Name = "Feijão 1Kg", Quantity = 3, QuantityUnitMeasure = Models.Enums.UnitMeasure.Un,Price = 7.49m, HasCaught = true },
                         new Product { Name = "Leite condensado", Quantity = 1, QuantityUnitMeasure = Models.Enums.UnitMeasure.Un, Price = 6.29m },
-                        new Product { Name = "Coxão Mole", Quantity = 1.5m, QuantityUnitMeasure = Models.Enums.UnitMeasure.Kg, Price = 29.80m }
+                        new Product { Name = "Coxão Mole", Quantity = 1.5m, QuantityUnitMeasure = Models.Enums.UnitMeasure.Kg, Price = 29.80m },
+                        new Product { Name = "Alcatara", Quantity = 1.5m, QuantityUnitMeasure = Models.Enums.UnitMeasure.Kg }
                     }
                 },
                 new ListToBuy()
