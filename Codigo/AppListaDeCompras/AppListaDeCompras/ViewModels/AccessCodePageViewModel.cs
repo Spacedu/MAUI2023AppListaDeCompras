3b3ed3c7a98a8020397eb3ba387b7b9c0891ce94
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-01-08 15:50:40 -0300

    S02E38 - Tela - Perfil - Código de Acesso

---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/ViewModels/AccessCodePageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AppListaDeCompras.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.ViewModels
+{
+    public partial class AccessCodePageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private User user;
+
+        [RelayCommand]
+        private void VerifyAccessCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
