0a9de335e521b6cf1fff882ab1039d99ac53ad0b
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-01-22 14:02:14 -0300

    S03E38 - Lista de Compras - Compartilhar - Parte 2

--- Codigo/AppListaDeCompras/AppListaDeCompras/ViewModels/Popups/ListToBuySharePageViewModel.cs
+++ Codigo/AppListaDeCompras/AppListaDeCompras/ViewModels/Popups/ListToBuySharePageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using AppListaDeCompras.Models;
+﻿using AppListaDeCompras.Libraries.Services;
+using AppListaDeCompras.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Mopups.Services;
@@ -13,12 +14,38 @@ namespace AppListaDeCompras.ViewModels.Popups
     public partial class ListToBuySharePageViewModel : ObservableObject
     {
         [ObservableProperty]
-        private ListToBuy _list = new ListToBuy() { /*Users = new List<User>()*/ };
+        private string? _email;
+
+        [ObservableProperty]
+        private ListToBuy _list = new ListToBuy() { };
 
         [RelayCommand]
         private void Close()
         {
             MopupService.Instance.PopAsync();
+        }
+        [RelayCommand]
+        private void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                App.Current!.MainPage!.DisplayAlert("Erro!", "Preencha o campo E-mail!", "OK");
+                return;
+            }
+
+            var realm = MongoDBAtlasService.GetMainThreadRealm();
+            var user = realm.All<User>().Where(a => a.Email == Email.Trim().ToLower()).FirstOrDefault();
+
+            if(user == null)
+            {
+                App.Current!.MainPage!.DisplayAlert("Não localizado.", "Usuário não localizado com o e-mail informado!", "OK");
+                return;
+            }
+
+            realm.WriteAsync(() => {
+                List.Users.Add(user);
+            });
+            
         }
     }
 }
