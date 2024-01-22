a5068bed35da244711891ed521f3925741cfa70b
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-01-22 13:31:02 -0300

    S03E37 - Lista de Compras - Compartilhar - Parte 1

---
+++ Codigo/AppListaDeCompras/AppListaDeCompras/Libraries/Converters/ShowIfUserLoggedConverter.cs
@@ -0,0 +1,23 @@
+﻿using AppListaDeCompras.Libraries.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.Libraries.Converters
+{
+    public class ShowIfUserLoggedConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return UserLoggedManager.ExistsUser();
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
