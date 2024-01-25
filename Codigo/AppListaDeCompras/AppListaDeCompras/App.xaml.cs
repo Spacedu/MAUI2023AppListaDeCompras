ecaf4d290f5e400c580dcb620465ed4d5f6660f3
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-01-25 13:53:17 -0300

    S03E42 - Lista de Compras - UI - Android - Correções da Interface Gráfica

--- Codigo/AppListaDeCompras/AppListaDeCompras/App.xaml.cs
+++ Codigo/AppListaDeCompras/AppListaDeCompras/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using AppListaDeCompras.Views;
-
+#if ANDROID
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+#endif
 namespace AppListaDeCompras
 {
     public partial class App : Application
@@ -7,8 +9,28 @@ namespace AppListaDeCompras
         public App()
         {
             InitializeComponent();
+            RemoveBorders();
 
             MainPage = VersionTracking.IsFirstLaunchEver ? new FirstPage() : new AppShell();
+        }
+
+        public void RemoveBorders()
+        {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("Borderless",(handler, view) => {
+#if ANDROID
+    handler.PlatformView.Background = null;
+    handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+    handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+#endif
+            });
+
+            Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("Borderless", (handler, view) => {
+#if ANDROID
+    handler.PlatformView.Background = null;
+    handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+    handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+#endif
+            });
         }
     }
 }
